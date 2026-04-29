bcaf4f466cc4a016918a2a4079c91f9ad9cf9ce8
itssaitej <160216479+itssaitej@users.noreply.github.com>
2026-04-29 13:36:36 +0530

    Update RegisterRequest.java

--- src/main/java/com/blood/donorApp/dto/RegisterRequest.java
+++ src/main/java/com/blood/donorApp/dto/RegisterRequest.java
@@ -7,7 +7,7 @@ public class RegisterRequest
 	private String username;
     private String password;
     private Long id;
-    
+
     private String name;
     private String bloodGroup;
     private String city;
